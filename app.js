105ca28f3d94a9729094aa37c2b6a9ae7c634f7a
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-02-26 14:28:17 +0800

    set appid appsecret

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
+    appId: "9d446f5a-3443-4df6-8fca-1d6861d6e5e3",
+    appPassword: "avpaaPRIB988#rdSGE93)-#",
     openIdMetadata: process.env.BotOpenIdMetadata
 });
 
