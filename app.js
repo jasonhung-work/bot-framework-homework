d4669ba159718f6c892d135c00b4cfb7250b5abd
LAPTOP-HU9RDDIO\洪國竣 <jason.hung0119@gmail.com>
2018-02-27 09:32:55 +0800

    no message

--- app.js
+++ app.js
@@ -64,18 +64,6 @@ var bot = new builder.UniversalBot(connector, [
 bot.dialog('language', [
     function (session) {
         session.send("請選擇您要使用的語言");
-        builder.Prompts.choice(session, "What's your preferred language?", "中文|英文|簡中", { listStyle: builder.ListStyle.button });
-    },
-    function (session, results) {
-        session.endDialogWithResult({
-            response: { language: results.response.entity }
-        });
-    }
-]);
-
-bot.dialog('isRepair', [
-    function (session) {
-        session.send("歡迎光臨大同世界科技０８００報修系統，您可以在這裡取得大同世界科技客服中心的服務");
         var msg = new builder.Message(session);
         msg.attachmentLayout(builder.AttachmentLayout.carousel)
         msg.attachments([
@@ -93,6 +81,18 @@ bot.dialog('isRepair', [
     },
     function (session, results) {
         console.log(results);
+        session.endDialogWithResult({
+            response: { language: results.response.entity }
+        });
+    }
+]);
+
+bot.dialog('isRepair', [
+    function (session) {
+        session.send("歡迎光臨大同世界科技０８００報修系統，您可以在這裡取得大同世界科技客服中心的服務");
+        builder.Prompts.choice(session, "請問您是要進行故障報修嗎?", "yes|no", { listStyle: 3 });
+    },
+    function (session, results) {
         if (results.response.entity == "yes") {
             session.dialogData.isRepair = "yes";
             builder.Prompts.number(session, "請輸入您的統一編號");
@@ -100,7 +100,7 @@ bot.dialog('isRepair', [
         else {
             session.send("謝謝您的光臨，願您一切順心，再見！");
             session.endDialogWithResult({
-                response: { isRepair: results.response.entity }
+                response: { isRepair: results.response.entity}
             });
         }
     },
@@ -130,6 +130,14 @@ bot.dialog('showShirts', function (session) {
                 builder.CardAction.imBack(session, "中文", "中文 (1)"),
                 builder.CardAction.imBack(session, "英文", "英文 (1)"),
                 builder.CardAction.imBack(session, "簡中", "簡中 (1)"),
+            ]),
+        new builder.HeroCard(session)
+            .title("Classic Gray T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
             ])
     ]);
     session.send(msg).endDialog();
